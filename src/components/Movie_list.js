7b1a6a33093b0cedd24a634afac4c772292776cd
hamza-boudefa <boudefahamza@gmail.com>
2023-05-22 20:47:39 +0100

    add new movie

--- src/components/Movie_list.js
+++ src/components/Movie_list.js
@@ -4,6 +4,8 @@ import Movies from './Movies'
 const Movie_list = ({aflem}) => {
   return (
     <div id='movies' style={{display:'flex' , flexWrap:"wrap" , justifyContent:'space-evenly'}} >
+      {/*  useing map function to iterate over each movie in array of object in the movies
+    and passing every single movie as props to movie card component */}
     {aflem.map(film=> <Movies film={film} />  )}
     </div>
   )
